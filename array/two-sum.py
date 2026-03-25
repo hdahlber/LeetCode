bad00d3c65dac39c72e03ae0897f1889b2ecc2d7
hdahlber <hdahlber@abo.fi>
2026-03-25 14:19:09 +0200

    LC-two-sum [Runtime: N/A; Memory: N/A]

---
+++ array/two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i,num in enumerate(nums):
+            if num >= target:
+                continue
+            else:
+                result = target - num
+                if result in seen:
+                    return [seen[result],i]
+                seen[num] = i
+            
+
+        
